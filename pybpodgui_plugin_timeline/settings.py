9297e10bf8e08d9ac130ec46c9e510dbc2d2f899
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2017-04-07 17:41:58 +0100

    Workaround for pyinstaller

--- pybpodgui_plugin_timeline/settings.py
+++ pybpodgui_plugin_timeline/settings.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 import os
+
+SETTINGS_PRIORITY = 80
+
+# THESE SETTINGS ARE NEEDED FOR PYSETTINGS
+PYFORMS_USE_QT5 = True
 
 TIMELINE_PLUGIN_ICON = os.path.join(os.path.dirname(__file__), 'resources', 'graph.png')
 
